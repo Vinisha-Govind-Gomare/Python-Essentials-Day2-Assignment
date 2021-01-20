b042a728beb97ec6302fb63d56ee2b34fedb2553
Vinisha-Govind-Gomare <72209665+Vinisha-Govind-Gomare@users.noreply.github.com>
2021-01-20 16:38:11 +0530

    Day2  Assignment Submitted

---
+++ main.py
@@ -0,0 +1,11 @@
+number=int(input("Enter the number:"))
+
+total= 0
+value= 1
+while(value<=number):
+    total=total+value
+    value=value+1
+    
+print("The Sum of natural numbers from 1 to {0}= {1}".format(number,total))    
+    
+    
